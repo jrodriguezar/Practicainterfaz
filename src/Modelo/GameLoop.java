a73466e34a3a33f2d47b0577a99480a9dbb9ed9f
Estudiante <Estudiante@10.203.0.152>
2018-10-03 08:52:22 -0500

    ADT lists

--- src/Modelo/GameLoop.java
+++ src/Modelo/GameLoop.java
@@ -2,25 +2,80 @@ package Modelo;
 
 import javafx.animation.AnimationTimer;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.SVGPath;
+import javafx.scene.shape.Shape;
 
 public class GameLoop extends AnimationTimer{
     private GraphicsContext gc;
     private Carro carro;
+    private Image fondo;
+    private Image gato;
+    private Image munieco;
+    private int x=0;
+    private int i=0;
     
     public GameLoop(GraphicsContext gc) {
         this.gc = gc;
-        this.carro = new Carro(0, 100);
+        this.carro = new Carro(0, 390);
+        this.fondo = new Image("imagenes/fondo.png");
+        this.gato =  new Image("imagenes/cats.gif");
+        this.munieco = new Image("imagenes/down0.png");
     }
     
     @Override
     public void handle(long l) {
         //Borrando el tablero
-        gc.clearRect(0, 0, 300, 300);
-        double xpoints[] = {carro.getChasis().getX(), carro.getChasis().getX()+12, carro.getChasis().getX()+17, carro.getChasis().getX()+35, carro.getChasis().getX()+40, carro.getChasis().getX()+50, carro.getChasis().getX()+50, carro.getChasis().getX(), carro.getChasis().getX()};
+        gc.clearRect(0, 0, 1024, 512);
+        
+        
+        //dibujar fondo
+        gc.drawImage(fondo, 0, 0);
+        //primer gato
+        gc.strokeRect(20+i, 20, 132, 80);
+        gc.drawImage(gato, 132*x, 0, 132, 80, 20+i, 20, 132, 80);
+        /*gc.drawImage(gato, 132, 0, 132, 80, 20, 20, 132, 80);
+        gc.drawImage(gato, 264, 0, 132, 80, 20, 20, 132, 80);*/
+        if (i%7 == 0) {
+            x++;
+            i++;
+        }else{
+            i++;
+        }
+        if (x==5) {
+            x=0;
+        }
+        int g = 0;
+        ///arreglar!!!!!
+        while(g<300){
+            for (int j = Math.random(); j < 100; j++) {
+                gc.setFill(Color.AQUA);
+                gc.fillOval(j, 0+i, 8, 8);
+                break;
+            }
+            g++;
+        }
+        //munieco
+        /*gc.drawImage(munieco, 200, 20);
+        gc.strokeRect(200, 20, 30, 39);*/
+        
+        //colision
+        /*Shape sgato = new Rectangle(20+i, 20, 132, 80);
+        Shape smen = new Rectangle(200, 20, 30, 39);
+        Shape interseccion = SVGPath.intersect(sgato, smen);//clase especial para las intersecciones
+        if (interseccion.getBoundsInLocal().getWidth() != -1) {
+            stop();//detiene todos los moviminetos
+            //se puede modificar a gusto, no necesariamente debe detenerse todos los movimientos
+        }*/
+        
+        /*double xpoints[] = {carro.getChasis().getX(), carro.getChasis().getX()+12, carro.getChasis().getX()+17, carro.getChasis().getX()+35, carro.getChasis().getX()+40, carro.getChasis().getX()+50, carro.getChasis().getX()+50, carro.getChasis().getX(), carro.getChasis().getX()};
         double ypoints[] = {carro.getChasis().getY(), carro.getChasis().getY(), carro.getChasis().getY()-15, carro.getChasis().getY()-15, carro.getChasis().getY(), carro.getChasis().getY(), carro.getChasis().getY()+8, carro.getChasis().getY()+8, carro.getChasis().getY()};
-        gc.strokePolygon(xpoints, ypoints, xpoints.length);
+        gc.setFill(Color.AZURE);
+        gc.fillPolygon(xpoints, ypoints, xpoints.length);
         gc.fillOval(carro.getLlantas(0).getX()+12, carro.getLlantas(0).getY()+8, 8, 8);
         gc.fillOval(carro.getLlantas(1).getX()+32, carro.getLlantas(1).getY()+8, 8, 8);
-        carro.mover();
+        carro.mover();*/
     }
 }
--- src/Modelo/Tablero.java
+++ src/Modelo/Tablero.java
@@ -1,5 +1,6 @@
 package Modelo;
 
+import javafx.scene.image.Image;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -18,9 +19,9 @@ public class Tablero extends Application {
     public void start(Stage stage) throws Exception {
         //Loyout
         Pane pane1 = new Pane();
-        Canvas canvas = new Canvas(300, 300);
+        Canvas canvas = new Canvas(1024, 512);
         pane1.getChildren().add(canvas);
-        Scene scene = new Scene(pane1, 300, 300, Color.WHITESMOKE);
+        Scene scene = new Scene(pane1, 1024, 512, Color.WHITESMOKE);
         //Referecia del lapiz para dibujar sobre
         //el canvas
         GraphicsContext gc = canvas.getGraphicsContext2D();
@@ -31,24 +32,6 @@ public class Tablero extends Application {
         stage.setTitle("Ejemplo canvas");
         stage.setScene(scene);
         stage.show();
-        
-        //Dibujar un rectangulo
-        //gc.setFill(Color.CHARTREUSE);//da el color para la variable gc
-        //gc.fillRect(10, 10, 20, 20);
-        
-        //Se crea un ovalo, dos primeras coordenadas los otros el radio en x y y
-        //gc.fillOval(30, 30, 50, 50);
-        
-        //Se dibuja una linea, las dos primeras son las cooordenadas d inicio y 
-        //los dos ultimos las coordenasdas de final
-        //gc.strokeLine(15, 15, 40, 40);
-        
-        //Se crea un poligono, para ello necesito las coordenasdas diferentes de
-        //x y y, ademas despues de crea como tal el poligono con estas
-        /*double xpoints[] = {10, 30, 20, 10};
-        double ypoints[] = {30, 30, 15, 30};
-        gc.strokePolygon(ypoints, xpoints, xpoints.length);//Al usar stroke la figura queda sin relleno
-        gc.strokeRect(30, 30, 15, 15);*/
     }
     
     public static void main(String[] args) {
